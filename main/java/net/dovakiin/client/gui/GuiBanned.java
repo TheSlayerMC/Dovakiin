d2eb3c6190599e9c9ec0bd67379769c1dc638d07
TheSlayerMC <brock.kerley@hotmail.com>
2014-03-09 12:46:09 +1030

    Still testing

--- main/java/net/dovakiin/client/gui/GuiBanned.java
+++ main/java/net/dovakiin/client/gui/GuiBanned.java
@@ -2,6 +2,7 @@ package net.dovakiin.client.gui;
 
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.client.GuiAccessDenied;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
@@ -14,7 +15,7 @@ public class GuiBanned extends GuiScreen{
 	public void draw(){
 		 this.drawDefaultBackground();
 		 int offset = Math.max(85 - 2 * 10, 10);
-		 this.drawCenteredString(this.fontRendererObj, "You have been banned from the mod 'Dovakiin'.", this.width / 2, offset, 0xFFFFFF);
+		 this.drawCenteredString(this.fontRendererObj, Minecraft.getMinecraft().thePlayer.getDisplayName() + " you have been banned from the mod 'Dovakiin'.", this.width / 2, offset, 0xFFFFFF);
 		 offset += 10;
 		 this.drawCenteredString(this.fontRendererObj, "Please contact The_SlayerMC for help.", this.width / 2, offset, 0xFFFFFF);
 		 offset += 10;
