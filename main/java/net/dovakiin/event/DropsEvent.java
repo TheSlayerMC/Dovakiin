d1681170f6275061ad1ce38800eab40073ef1bae
TheSlayerMC <brock.kerley@hotmail.com>
2014-03-11 18:11:52 +1030

    More work

--- main/java/net/dovakiin/event/DropsEvent.java
+++ main/java/net/dovakiin/event/DropsEvent.java
@@ -1,22 +1,20 @@
 package net.dovakiin.event;
 
-import java.util.List;
 import java.util.Random;
 
 import net.dovakiin.Dovakiin;
 import net.dovakiin.util.Utils;
-import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -24,7 +22,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class DropsEvent {
 
 	private Random rand = new Random();
-
+	Entity e = null; 
 	@SubscribeEvent
 	public void onDrop(LivingDropsEvent event){
 		EntityLivingBase e = event.entityLiving;
@@ -38,12 +36,12 @@ public class DropsEvent {
 
 	@SubscribeEvent
 	public void onKilledMob(LivingDeathEvent event){
-		Random rand = new Random();
+		e = event.entity;
 		if(!event.entity.worldObj.isRemote){
-			if(isHoldingSword(Minecraft.getMinecraft().thePlayer)){
+			if(event.entity instanceof EntityMob && isHoldingSword()){
 				Dovakiin.swordLevel += rand.nextInt(1) + 0.3F;
 				
-				if(Dovakiin.swordLevel <= 10){
+				if(Dovakiin.swordLevel <= 10 && ((ItemSword)Items.diamond_sword).getMaxDamage() > 10000){
 					((ItemSword)Items.diamond_sword).setMaxDamage(((ItemSword)Items.diamond_sword).getMaxDamage() + (int)Dovakiin.swordLevel + 300);
 					((ItemSword)Items.diamond_sword).setTextureName(Utils.PREFIX + "dragonEssence");
 				}
@@ -56,7 +54,7 @@ public class DropsEvent {
 		}
 	}
 
-	private boolean isHoldingSword(EntityPlayer player){
-		return player.getHeldItem().getItem() instanceof ItemSword;
+	private boolean isHoldingSword(){
+		return ((EntityPlayer)e).getHeldItem().getItem() instanceof ItemSword;
 	}
 }
